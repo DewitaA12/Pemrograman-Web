2d549d2b254605a43d2f172758732faf2884fe68
DewitaA12 <xsclthzn@gmail.com>
2024-10-03 08:30:36 +0700

    Associative array; styling tabel

--- learnPhp/PHP2/array_2.php
+++ learnPhp/PHP2/array_2.php
@@ -1,22 +1,48 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title></title>
-    
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        th, td {
+            border: 1px solid #ddd;
+            padding: 8px;
+            text-align: left;
+        }
+
+        tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
+
+        th {
+            background-color: #5227CC;
+            color: white;
+        }
+    </style>
 </head>
 <body>
     <?php
-    $Dosen = [
+     $Dosen = [
         'nama' => 'Elok Nur Hamdana',
         'domisili' => 'Malang',
         'jenis_kelamin' => 'Perempuan'
     ];
-
-    echo "Nama : " . $Dosen['nama'] . "<br>";
-    echo "Domisili : " . $Dosen['domisili'] . "<br>";
-    echo "Jenis Kelamin : " . $Dosen['jenis_kelamin'] . "<br>";
     ?>
+
+    <table>
+        <tr>
+            <th>Nama</th>
+            <th>Domisili</th>
+            <th>Jenis Kelamin</th>
+        </tr>
+        <tr>
+            <td><?php echo $Dosen['nama']; ?></td>
+            <td><?php echo $Dosen['domisili']; ?></td>
+            <td><?php echo $Dosen['jenis_kelamin']; ?></td>
+        </tr>
+    </table>
 </body>
 </html>
