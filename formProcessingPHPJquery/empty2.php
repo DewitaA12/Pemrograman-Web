890938fbeff049595198ef9c6a7ea22801f2a615
DewitaA12 <xsclthzn@gmail.com>
2024-11-17 20:11:38 +0700

    empty function:check variabel

---
+++ formProcessingPHPJquery/empty2.php
@@ -0,0 +1,10 @@
+<?php
+
+$myArray = array(); // Array kosong
+if (empty($nonExistentVar)) {
+    echo "Variabel tidak terdefinisi atau kosong.";
+} else {
+    echo "Variabel terdefinisi dan tidak kosong.";
+}
+
+?>
